8e1f4ab7ceabb3fdfce1884057e910383b36a94c
datlt2306 <datlt2306@gmail.com>
2022-01-10 17:06:45 +0700

    update navigo

--- src/main.js
+++ src/main.js
@@ -1,20 +1,24 @@
 import Navigo from "navigo";
+import HomePage from "./pages/home";
 
-const router = new Navigo();
+const router = new Navigo("/", { linksSelector: "a" });
+const print = (content) => {
+    document.getElementById("app").innerHTML = content;
+};
 
 router.on({
     "/": () => {
-        console.log("Home Page");
+        print(HomePage.render());
     },
     "/about": () => {
-        console.log("About Page");
+        print("About Page");
     },
     "/product": () => {
-        console.log("Product Page");
+        print("Product Page");
     },
 });
 
-router.notFound(() => console.log("Not Found Page"));
+router.notFound(() => print("Not Found Page"));
 
 router.resolve();
 
