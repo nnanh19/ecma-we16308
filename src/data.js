1d5000d8b9ef946b916c42a368230ffd3a927b3e
datlt2306 <datlt2306@gmail.com>
2022-01-10 18:17:53 +0700

    update

---
+++ src/data.js
@@ -0,0 +1,6 @@
+const data = [
+    { id: 1, title: "Tieu de 1", desc: "Mo ta san pham 1" },
+    { id: 2, title: "Tieu de 2", desc: "Mo ta san pham 3" },
+    { id: 3, title: "Tieu de 2", desc: "Mo ta san pham 4" },
+];
+export default data;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,15 @@
+import News from "../components/news";
+
 const HomePage = {
     render() {
-        return `Home Page in object`;
+        return /* html */`
+            <div class="banner">
+                <img src="https://picsum.photos/1500/400" />
+            </div>
+            <div class="news">
+                ${News.render()}
+            </div>
+        `;
     },
 };
 export default HomePage;
